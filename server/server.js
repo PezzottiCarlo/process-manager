af208cdc56e5d66245e7b4e8fa730812e5ffc6d2
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-04-26 13:40:39 +0200

    fix: :bug: fix using programmatic api

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const PM2 = require('./util/pm2');
 let pm2 = new PM2();
 
 async function init() {
-    console.log(await pm2.extractInfo(await pm2.getList()))
+    let list = await pm2.extractInfo(await pm2.getList());
+    console.log(list);
 }
 init()
--- server/util/pm2.js
+++ server/util/pm2.js
@@ -1,29 +1,45 @@
 const { exec } = require("child_process");
+const pm2 = require("pm2");
 
 class PM2 {
-    constructor() { }
-    async getList() {
-        //return require("./pm2-example.json");
-        let list = await new Promise((resolve, reject) => {
-            exec('pm2 jlist', (err, stdout, stderr) => {
+    constructor() {
+        if(!this.init()) {
+            throw new Error("PM2 not connected");
+        }
+    }
+
+    async init() {
+        let connect = await new Promise((resolve, reject) => {
+            pm2.connect((err) => {
                 if (err) {
                     reject(err);
                 }
-                resolve(stdout);
+                resolve(true);
             });
         })
-        try {
-            return JSON.parse(list);
-        } catch (e) {
-            return [];
+        return connect;
+    }
+
+    async getList() {
+        let list = await new Promise((resolve, reject) => {
+            pm2.list((err, list) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(list);
+            });
+        })
+        if(list.length > 0) {
+            return list;
         }
+        return [];
     }
     async extractInfo(list) {
         let result = [];
         for (let i = 0; i < list.length; i++) {
             let item = list[i];
-            let {pid,name,pm_id} = item;
-            let {node_version,restart_time,pm_uptime,PWD} = item.pm2_env;
+            let { pid, name, pm_id } = item;
+            let { node_version, restart_time, pm_uptime, pm_cwd } = item.pm2_env;
             result.push({
                 pid,
                 name,
@@ -31,18 +47,58 @@ class PM2 {
                 node_version,
                 restart_time,
                 pm_uptime,
-                PWD,
-                recommendedName:this.recommendedName(PWD,"webserver")
+                pm_cwd,
+                recommendedName: this.recommendedName(pm_cwd, "Projects")
             })
         }
         return result;
     }
-    recommendedName(pwd,rootFolder) {
-        let name = pwd.split(rootFolder+"/")[1];
-        if(name.indexOf("/")>-1){
-            name = name.split("/")[0];
+    recommendedName(pwd, rootFolder) {
+        let name = pwd.split(rootFolder + "/")[1];
+        if (name) {
+            if (name.indexOf("/") > -1) {
+                name = name.split("/")[0];
+            }
+            return name;
         }
-        return name;
+        return "";
+    }
+
+    //refact using programmatic api
+    stop(pm_id) {
+        let { err, stdout, stderr } = exec('pm2 stop ' + pm_id);
+        if (err) {
+            return err;
+        }
+        return true;
+    }
+
+    start(pm_id) {
+        let { err, stdout, stderr } = exec('pm2 start ' + pm_id);
+        if (err) {
+            return err;
+        }
+        return true;
+    }
+
+    restart(pm_id) {
+        let { err, stdout, stderr } = exec('pm2 restart ' + pm_id);
+        if (err) {
+            return err;
+        }
+        return true;
+    }
+
+    delete(pm_id) {
+        let { err, stdout, stderr } = exec('pm2 delete ' + pm_id);
+        if (err) {
+            return err;
+        }
+        return true;
+    }
+
+    monit(pm_id) {
+
     }
 }
 
