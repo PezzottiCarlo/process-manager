8c04559fa68ba73df870a3424b5e769faab76959
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-04-26 23:20:57 +0200

    style: :lipstick: remove id from ui

--- client/src/components/Process/Process.js
+++ client/src/components/Process/Process.js
@@ -10,7 +10,6 @@ const Process = (param) => {
 
     return (
         <div className="process">
-            <div className="process-id small">{process.pm_id}</div>
             <div className="process-name big">{process.name}</div>
             <div className="process-uptime medium">{Util.milliToTime(process.pm_uptime)}</div>
             <div className="process-uptime small"><FiRotateCcw />{process.restart_time}</div>
