5713af0145da1c4e275ad33332a6be6ca739e9ca
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-04-27 20:10:58 +0200

    style: :lipstick: fix ui

--- client/src/components/Process/Process.js
+++ client/src/components/Process/Process.js
@@ -8,10 +8,10 @@ const Process = ({process}) => {
     return (
         <div className="process">
             <div className={`process-name big ${(process.status === "online") ? "active" : "inactive"}`}>{process.name}</div>
-            <div className="process-uptime medium">{Util.milliToTime(process.pm_uptime)}</div>
+            <div className="process-uptime small">{Util.milliToTime(process.pm_uptime)}</div>
             <div className="process-uptime small"><FiRotateCcw />{process.restart_time}</div>
             <div className="process-watch small">{process.watch ? (<FiEye />) : (<FiEyeOff />)}</div>
-            <div className="process-monit  collapsible icon small monit"><FiMonitor /></div>
+            <div className="process-monit collapsible icon small monit"><FiMonitor /></div>
             <div className="process-restart  collapsible icon small restart"><FiRotateCcw /></div>
             <div className="process-stop  collapsible icon small stop"><FiPower /></div>
         </div>
