0307bce590cbb7212de664c5d010bad1e410c663
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-04-28 17:40:45 +0200

    feat: :sparkles: real life monit

--- client/src/components/Process/Process.js
+++ client/src/components/Process/Process.js
@@ -1,6 +1,6 @@
 import "./Process.css";
 import Util from "./util.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FiMonitor, FiPower, FiRotateCcw, FiEye, FiEyeOff } from "react-icons/fi";
 
 
@@ -11,11 +11,13 @@ const Process = ({ process }) => {
     const [watch, setWatch] = useState(process.watch);
     const [logs, setLogs] = useState([]);
 
-    const restart = async (pm_id,options = {}) => {
+    let updateMonit;
+
+    const restart = async (pm_id, options = {}) => {
         setStatus("changing");
-        let status = await Util.restart(pm_id,options);
+        let status = await Util.restart(pm_id, options);
         if (status) {
-            if(options.watch || !options.watch){
+            if (options.watch || !options.watch) {
                 setWatch(!watch);
             }
             setStatus(status);
@@ -31,16 +33,21 @@ const Process = ({ process }) => {
     }
 
     const monit = async (pm_name, length) => {
-        if(showLog){
-            return setShowLog(false);
-        }
         let monit = await Util.monit(pm_name, length);
         if (monit) {
-            console.log(monit);
             setShowLog(true);
             setLogs(monit.filter(line => line.length > 0));
         }
+    }
 
+    const handleMonit = async (pm_name, length) => {
+        if (showLog) {
+            setShowLog(false);
+            clearInterval(updateMonit);
+        } else {
+            monit(pm_name, length);
+            updateMonit = setInterval(() => monit(pm_name, length), length);
+        }
     }
 
     return (
@@ -49,19 +56,19 @@ const Process = ({ process }) => {
                 <div className={`process-name big ${status}`}>{process.name}</div>
                 <div className="process-uptime small">{Util.milliToTime(process.pm_uptime)}</div>
                 <div className="process-uptime small"><FiRotateCcw />{process.restart_time}</div>
-                <div className="process-watch small" onClick={() => { restart(process.pm_id,{watch:!watch}) }}>{watch ? (<FiEye className="small-icon"/>) : (<FiEyeOff className="small-icon" small-/>)}</div>
-                <div className="process-monit collapsible icon small monit" onClick={() => { monit(process.name, 15) }}><FiMonitor /></div>
+                <div className="process-watch small" onClick={() => { restart(process.pm_id, { watch: !watch }) }}>{watch ? (<FiEye className="small-icon" />) : (<FiEyeOff className="small-icon" small- />)}</div>
+                <div className="process-monit collapsible icon small monit" onClick={() => { handleMonit(process.name, 15) }}><FiMonitor /></div>
                 <div className="process-restart  collapsible icon small restart" onClick={() => { restart(process.pm_id) }}><FiRotateCcw /></div>
                 <div className="process-stop  collapsible icon small stop" onClick={() => { stop(process.pm_id) }}><FiPower /></div>
             </div>
             {(showLog) ? (
-            <div className="process-logs">
-                {
-                    logs.map((log, index) => {
-                        return <p className="code">{log}</p>
-                    })
-                }
-            </div>) : (null)}
+                <div className="process-logs">
+                    {
+                        logs.map((log, index) => {
+                            return <p className="code">{log}</p>
+                        })
+                    }
+                </div>) : (null)}
         </div>
     );
 }
